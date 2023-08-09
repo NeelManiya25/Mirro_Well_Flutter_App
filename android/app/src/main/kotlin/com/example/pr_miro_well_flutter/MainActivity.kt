f446d9befe44a9659a199f0b6999dbd135a40474
neelmaniya <neelmaniya81@gmail.com>
2023-08-09 08:16:27 +0530

    09-08-2023

---
+++ android/app/src/main/kotlin/com/example/pr_miro_well_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pr_miro_well_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/providers/connectivity_provider.dart
@@ -0,0 +1,33 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flutter/foundation.dart';
+import 'package:pr_miro_well_flutter/modals/connectivity_model.dart';
+
+
+
+
+class ConnectivityProvier extends ChangeNotifier {
+  Connectivity connectivity = Connectivity();
+
+  ConnectivityModel connectivityModel =
+      ConnectivityModel(ConnectivityStatus: "Waiting");
+
+  void checkInternetConnectvity() {
+    connectivityModel.ConnectivityStream = connectivity.onConnectivityChanged
+        .listen((ConnectivityResult connectivityResult) {
+      switch (connectivityResult) {
+        case ConnectivityResult.wifi:
+          connectivityModel.ConnectivityStatus = "wifi";
+          notifyListeners();
+          break;
+        case ConnectivityResult.mobile:
+          connectivityModel.ConnectivityStatus = "mobile";
+          notifyListeners();
+          break;
+        default:
+          connectivityModel.ConnectivityStatus = "Waiting..";
+          notifyListeners();
+          break;
+      }
+    });
+  }
+}
